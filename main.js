57d1865b08eedc20dccd9047a8feb0ab56ce3b5c
Steven Winter <stevenwinter.me@gmail.com>
2020-07-26 19:57:56 -0500

    Completed contact form by adding main.js and editing index.html and styles.css.

---
+++ main.js
@@ -0,0 +1,49 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBqAHRdDZpNKlJKQBK9EkMB85-l0Tscx7Y",
+    authDomain: "portfolio-form-6b596.firebaseapp.com",
+    databaseURL: "https://portfolio-form-6b596.firebaseio.com",
+    projectId: "portfolio-form-6b596",
+    storageBucket: "portfolio-form-6b596.appspot.com",
+    messagingSenderId: "848003063752",
+    appId: "1:848003063752:web:1f0c5b7bf0e2aae0df6afd",
+    measurementId: "G-6ZYP2XFKJP"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const firestore = firebase.firestore();
+
+const form = document.querySelector('#contact');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const message = document.querySelector('#message');
+
+
+const db = firestore.collection('portfolio-contactform');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const nameInput = name.value;
+    const emailInput = email.value;
+    const messageInput = message.value;
+    const date= new Date();
+    const alert = document.querySelector('.alert');
+    alert.style.display = 'block';
+    setTimeout(function(){
+        alert.style.display = 'none';
+    }, 3000);
+
+    db.doc().set({
+        date: date,
+        name: nameInput,
+        email: emailInput,
+        message: messageInput
+    }).then(function() {
+        console.log('Data saved');
+    }).catch(function(error) {
+        console.log(error);
+    })
+
+    form.reset();
+});
