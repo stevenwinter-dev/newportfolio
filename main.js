572863d0db286c01c9bf405fb0aea103ea226a91
Steven Winter <stevenwinter.me@gmail.com>
2020-08-01 19:11:35 -0500

    Added fade in animation to mobile nav.

--- main.js
+++ main.js
@@ -47,3 +47,15 @@ form.addEventListener('submit', function(e) {
 
     form.reset();
 });
+
+const checkpoint = 200;
+ 
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= checkpoint) {
+    opacity = 0;
+  } else {
+    opacity = 1;
+  }
+  document.querySelector(".mobile-nav").style.opacity = opacity;
+});
